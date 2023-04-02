ff911ae1083fb0bce377c4cb888e3461784f5fba
Coder2287569013 <101576295+Coder2287569013@users.noreply.github.com>
2023-04-02 19:15:58 +0300

    Add files via upload

--- map.py
+++ map.py
@@ -3,8 +3,6 @@ import pygame
 pygame.init()
 
 #variables
-sc_w = 1200
-sc_h = 800
 fps = 60
 game = True
 block_x = 0
@@ -16,6 +14,7 @@ dirt_img = pygame.image.load("dirt.png")
 #--------------------------------------
 
 sc = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+sc_w, sc_h = sc.get_width(), sc.get_height()
 clock = pygame.time.Clock()
 
 #classes
@@ -27,42 +26,39 @@ class GameSprite(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = x, y
     def draw(self):
         sc.blit(self.image, (self.rect.x, self.rect.y))
+    def draw_sur(self,surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
 
 class Camera():
     def __init__(self):
         self.speed = 10
+        self.x = 0
+        self.y = 0
     def movement(self):
         keys_input = pygame.key.get_pressed()
-        if keys_input[pygame.K_w]:
-            for block in world_map:
-                block.rect.y += self.speed
-        if keys_input[pygame.K_a]:
-            for block in world_map:
-                block.rect.x += self.speed
-        if keys_input[pygame.K_s]:
-            for block in world_map:
-                block.rect.y -= self.speed
-        if keys_input[pygame.K_d]:
-            for block in world_map:
-                block.rect.x -= self.speed
+        for line in world_map:
+            for block in line:
+                if keys_input[pygame.K_w] and world_map[0][0].rect.top < 0:
+                    block.rect.y += self.speed
+                if keys_input[pygame.K_a] and world_map[0][0].rect.left < 0:
+                    block.rect.x += self.speed
+                if keys_input[pygame.K_s] and world_map[0][49].rect.bottom > sc_h:
+                    block.rect.y -= self.speed
+                if keys_input[pygame.K_d] and world_map[49][0].rect.right > sc_w+100:
+                    block.rect.x -= self.speed
 #--------------------------------------
 camera = Camera()
-world_map = []
-for x in range(200):
-    for y in range(200):
-        dirt = GameSprite(block_x, block_y,100,100,dirt_img)
-        world_map.append(dirt)
-        block_x += 50
-    block_x = 0
-    block_y += 50
+tileSize = 100
+world_map = [[GameSprite(tileSize*i,tileSize*y,tileSize,tileSize,dirt_img) for y in range(50)] for i in range(50)]
 while game:
     sc.fill(BLACK)
-    for block in world_map:
-        block.draw()
+    for line in world_map:
+        for block in line:
+            block.draw()
     camera.movement()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             game = False
-    pygame.display.update()
+    pygame.display.flip()
     clock.tick(fps)
