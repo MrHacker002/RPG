217d4636a64cab33a9179036e0d432d92e0947af
Coder2287569013 <101576295+Coder2287569013@users.noreply.github.com>
2023-04-03 18:22:28 +0300

    Add files via upload

--- map.py
+++ map.py
@@ -4,9 +4,12 @@ import os
 
 pygame.init()
 
+sc = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+
 #variables
-sc_w = 1200
-sc_h = 800
+sc_w = sc.get_width()
+sc_h = sc.get_height()
 fps = 60
 game = True
 block_x = 0
@@ -15,13 +18,9 @@ BLACK = (0,0,0)
 #--------------------------------------
 path_img = os.path.join(os.path.abspath(__file__+'\..'), "assets")
 #game assets
-dirt_img = pygame.image.load(os.path.join(path_img, "dirt.jpg"))
+dirt_img = pygame.image.load(os.path.join(path_img, "dirt.png"))
 player_img = pygame.image.load(os.path.join(path_img, "player.jpg"))
-#--------------------------------------
-
-sc = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-clock = pygame.time.Clock()
-
+#-------------------------------------
 #classes
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, image):
@@ -35,20 +34,19 @@ class GameSprite(pygame.sprite.Sprite):
         surface.blit(self.image, (self.rect.x, self.rect.y))
 
 class Camera():
-    def __init__(self):
+    def __init__(self,x,y):
         self.speed = 15
+        self.rect = pygame.Rect(x,y,0,0)
     def movement(self, mouse_pos_x, mouse_pos_y):
         keys_input = pygame.key.get_pressed()
-        for line in world_map:
-            for block in line:
-                if keys_input[pygame.K_w] and world_map[0][0].rect.top < 0:
-                    block.rect.y += self.speed
-                if keys_input[pygame.K_a] and world_map[0][0].rect.left < 0:
-                    block.rect.x += self.speed
-                if keys_input[pygame.K_s] and world_map[0][49].rect.bottom > sc_h:
-                    block.rect.y -= self.speed
-                if keys_input[pygame.K_d] and world_map[49][0].rect.right > sc_w+100:
-                    block.rect.x -= self.speed
+        if keys_input[pygame.K_w] and self.rect.top > 0:
+            self.rect.y += self.speed * -1
+        if keys_input[pygame.K_s] and self.rect.bottom < world_map_h-sc_h:
+            self.rect.y += self.speed
+        if keys_input[pygame.K_d] and self.rect.right < world_map_w-sc_w:
+            self.rect.x += self.speed
+        if keys_input[pygame.K_a] and self.rect.left > 0:
+            self.rect.x += self.speed*-1
 class Player(GameSprite):
     def __init__(self, x, y, width, height, image, speed):
         super().__init__(x, y, width, height, image)
@@ -78,7 +76,6 @@ class Player(GameSprite):
                 self.frames = self.dist_y//self.speed
                 self.speed_x = self.dist_x//self.frames
 
-
             if self.rect.x < mouse_pos_x:
                 self.rect.x += self.speed_x
             else:
@@ -99,22 +96,31 @@ class Player(GameSprite):
 
 #--------------------------------------
 #Создание экземпляров классов
-camera = Camera()
+camera = Camera(0,0)
 hero = Player(800, 400, 100, 100, player_img, 15)
 # Создание карты
 tileSize = 100
-world_map = [[GameSprite(tileSize*i,tileSize*y,tileSize,tileSize,dirt_img) for y in range(50)] for i in range(50)]
+world_map = [["d" for x in range(90)] for y in range(90)]
+world_map_w = len(world_map[0])*tileSize
+world_map_h = len(world_map)*tileSize
 
 mouse_pos_x = 0
 mouse_pos_y = 0
 while game:
     sc.fill(BLACK)
+    y = 0
     for line in world_map:
+        x = 0
         for block in line:
-            block.draw()
-    camera.movement()
-    hero.move(mouse_pos_x, mouse_pos_y)
+            if (x*tileSize+camera.rect.x*-1 <= sc_w+100 and y*tileSize+camera.rect.y*-1 <= sc_h+100):
+                if (x*tileSize+camera.rect.x*-1 >= 0-tileSize and y*tileSize+camera.rect.y*-1 >= 0-tileSize):
+                    if block == "d":
+                        sc.blit(pygame.transform.scale(dirt_img, (tileSize, tileSize)), (x*tileSize+camera.rect.x*-1,y*tileSize+camera.rect.y*-1))
+            x+=1
+        y+= 1
+    camera.movement(mouse_pos_x, mouse_pos_y)
     hero.draw()
+    hero.move(mouse_pos_x, mouse_pos_y)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -126,4 +132,4 @@ while game:
                 hero.is_moving = True
     pygame.display.update()
     clock.tick(fps)
-
+    # print(clock.get_fps())
